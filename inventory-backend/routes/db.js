68cb70bbaf72cff9a9cbc206db8d58d726304c26
thinkC <tunde.oyewo@gmail.com>
2023-08-17 17:26:39 +0100

    modified db

--- inventory-backend/routes/db.js
+++ inventory-backend/routes/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '',
+  password: 'Slb01234*',
   database: 'inventory',
   connectionLimit: 10, // Adjust as needed
 });
