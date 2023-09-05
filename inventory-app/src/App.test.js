0115e90069bd98c00637788ea18c471ce9121bb7
thinkc <tunde.oyewo@gmail.com>
2023-09-05 22:35:32 +0100

    add a test branch

--- inventory-app/src/App.test.js
+++ inventory-app/src/App.test.js
@@ -3,6 +3,17 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Inventory Items/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders Inventory Items header', () => {
+//   const { getByText } = render(<App />);
+//   const headerElement = getByText(/Inventory Items/i);
+//   expect(headerElement).toBeInTheDocument();
+// });
